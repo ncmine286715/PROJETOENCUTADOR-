0aacc5a68332245c5aa7ced4a4dcf25a65aa480c
ncmine286715 <rosidomingos032@gmail.com>
2026-02-23 09:34:23 -0300

    Create links.js

---
+++ links.js
@@ -0,0 +1,15 @@
+const LINKS_DATABASE = {
+    // FORMATO: "id-do-video": { name: "Nome do Addon", url: "Link de Download" }
+    "carro-v1": {
+        name: "Addon de Carro Realista v1",
+        url: "https://seulinkdedownload.com/arquivo1"
+    },
+    "arma-3d": {
+        name: "Pack de Armas 3D v2",
+        url: "https://seulinkdedownload.com/arquivo2"
+    },
+    "mutante": {
+        name: "Addon de Mutantes RPG",
+        url: "https://seulinkdedownload.com/arquivo3"
+    }
+};
